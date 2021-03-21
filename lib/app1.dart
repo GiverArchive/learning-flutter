35be89d5177bd10dd72514095df9e630aecb6d7b
GiverPlay007 <play.giver.007@gmail.com>
2021-03-20 21:29:51 -0300

    Projeto criado

---
+++ lib/app1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          'Flutterando',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 50.0)
+        )
+      )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'App1.dart';
+
+void main() {
+  runApp(App());
+}
